c47b62c415189a82eaa7d0735f5a14c08f73877c
Sean Huver <shuver@nvidia.com>
2026-03-21 12:58:32 -0700

    Add interval annotations backend scaffolding

--- cvat/apps/engine/serializers.py
+++ cvat/apps/engine/serializers.py
@@ -2732,6 +2732,7 @@ class TaskWriteSerializer(WriteOnceMixin, serializers.ModelSerializer, OrgTransf
                     (models.LabeledTrackAttributeVal, 'track'),
                     (models.LabeledShapeAttributeVal, 'shape'),
                     (models.LabeledImageAttributeVal, 'image'),
+                    (models.LabeledIntervalAttributeVal, 'interval'),
                     (models.TrackedShapeAttributeVal, 'shape__track')
                 ):
                     model.objects.filter(**{
@@ -2740,7 +2741,7 @@ class TaskWriteSerializer(WriteOnceMixin, serializers.ModelSerializer, OrgTransf
                         'spec': old_attr
                     }).update(spec=new_attr)
 
-            for model in (models.LabeledTrack, models.LabeledShape, models.LabeledImage):
+            for model in (models.LabeledTrack, models.LabeledShape, models.LabeledImage, models.LabeledInterval):
                 model.objects.filter(job__segment__task=instance, label=old_label).update(
                     label=new_label
                 )
@@ -3315,6 +3316,15 @@ class AnnotationSerializer(serializers.Serializer):
 class LabeledImageSerializer(AnnotationSerializer):
     attributes = AttributeValSerializer(many=True, default=[])
 
+class LabeledIntervalSerializer(AnnotationSerializer):
+    end_frame = serializers.IntegerField(min_value=0)
+    attributes = AttributeValSerializer(many=True, default=[])
+
+    def validate(self, attrs):
+        if attrs["end_frame"] < attrs["frame"]:
+            raise serializers.ValidationError({"end_frame": "must be greater than or equal to frame"})
+        return attrs
+
 class OptimizedFloatListField(serializers.ListField):
     '''Default ListField is extremely slow when try to process long lists of points'''
 
@@ -3423,6 +3433,17 @@ class LabeledImageSerializerFromDB(serializers.BaseSerializer):
 
         return convert_tag(instance)
 
+class LabeledIntervalSerializerFromDB(serializers.BaseSerializer):
+    # Use this serializer to export data from the database
+    # Because default DRF serializer is too slow on huge collections
+    def to_representation(self, instance):
+        def convert_interval(interval):
+            result = _convert_annotation(interval, ['id', 'label_id', 'frame', 'end_frame', 'group', 'source'])
+            result['attributes'] = _convert_attributes(interval['attributes'])
+            return result
+
+        return convert_interval(instance)
+
 class LabeledShapeSerializerFromDB(serializers.BaseSerializer):
     # Use this serializer to export data from the database
     # Because default DRF serializer is too slow on huge collections
@@ -3474,6 +3495,7 @@ class LabeledTrackSerializer(SubLabeledTrackSerializer):
 class LabeledDataSerializer(serializers.Serializer):
     version = serializers.IntegerField(default=0) # TODO: remove
     tags   = LabeledImageSerializer(many=True, default=[])
+    intervals = LabeledIntervalSerializer(many=True, default=[])
     shapes = LabeledShapeSerializer(many=True, default=[])
     tracks = LabeledTrackSerializer(many=True, default=[])
 
